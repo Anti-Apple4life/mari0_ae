781fb4b5aa0c518f143f3706761fed2422eaf77c
Anti-Apple4life <59315931+Anti-Apple4life@users.noreply.github.com>
2023-02-14 11:33:15 -0500

    Update intro.lua

--- intro.lua
+++ intro.lua
@@ -62,20 +62,20 @@ function intro_draw()
 			
 			if introprogress > introfadetime+0.3 and introprogress < introduration - introfadetime then
 				local y = (introprogress-0.2-introfadetime) / (introduration-2*introfadetime) * 206 * 5
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 110*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 110*scale)
 				love.graphics.setScissor(0, screenheight/2+150*logoscale - y, screenwidth, y)
 				love.graphics.setColor(100, 100, 100, a)
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 110*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 110*scale)
 				love.graphics.setColor(255, 255, 255, a)
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 109*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 109*scale)
 				love.graphics.setScissor()
 			elseif introprogress >= introduration - introfadetime then
 				love.graphics.setColor(100, 100, 100, a)
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 110*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 110*scale)
 				love.graphics.setColor(255, 255, 255, a)
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 109*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 109*scale)
 			else
-				properprint("stys.eu", ((width*16)*scale)/2-string.len("stys.eu")*4*scale, 110*scale)
+				properprint("Unblockable", ((width*16)*scale)/2-string.len("Unblockable")*4*scale, 110*scale)
 			end
 		else
 			if introprogress > introfadetime+0.3 and introprogress < introduration - introfadetime then
